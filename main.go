dc4abd9418be4000b9dd93ed2d8d6920fbc70a51
Evgeniy <spkissaal@gmail.com>
2025-01-09 20:15:28 +0300

    minor fix package name

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 
 func main() {
 	logger := logme.NewLogMe()
-	logger.SetLevel()
+	logger.SetLevel().Warning()
 	logger.AddOutput().ConsoleOutput()
 	logger.AddOutput().FileOutput("mylog.txt")
 	logger.AddField("server", "her")
 	logger.AddField("AUTHOR", "ME")
 	//logger.Info(12, 21, 219, 45)
 	logger.Info("Из-за леса, из-за гор, показал мужик топор..")
-	//logger.Warning("..но не просто показал!..")
-	//logger.Debug("(прищурься)")
-	//logger.Critical("его к хую привязал!")
+	logger.Warning("..но не просто показал!..")
+	logger.Debug("(прищурься)")
+	logger.Critical("его к хую привязал!")
 }
